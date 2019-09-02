5a006cd0ebb69f572721063ba1151c26c0cfe375
★刘☆备★ <www.782771968@qq.com>
2019-09-02 19:50:54 +0800

    swagger2增加

--- src/main/java/com/fosu/miki/crm/CrmApplication.java
+++ src/main/java/com/fosu/miki/crm/CrmApplication.java
@@ -2,15 +2,29 @@ package com.fosu.miki.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.fosu.miki.crm.mapper")
+@EnableSwagger2
 public class CrmApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CrmApplication.class, args);
     }
 
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
